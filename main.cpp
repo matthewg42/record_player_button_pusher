fcb4b7e57b8d63f5e538cdefa1abb3b5b4e65174
Matthew Gates <matthewg42@gmail.com>
2016-02-01 18:43:51 +0000

    tweak servo arm positions

--- main.cpp
+++ main.cpp
@@ -7,8 +7,8 @@ Servo sg90;
 #define LED_PIN				13
 #define SERVO_PIN			11	
 #define REST_POS			40
-#define PRESS_POS			63
-#define PRESS_DELAY_MS		100
+#define PRESS_POS			68
+#define PRESS_DELAY_MS		50
 #define MOVE_DELAY_MS		15
 #define REST_DELAY_SEC		(8*60)	
 #define HEARTBEAT_DELAY 	2000
